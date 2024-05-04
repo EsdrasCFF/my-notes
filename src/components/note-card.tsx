b6318c35c439e7e348cd180a6bf7c63cf76d2e93
EsdrasCFF <ecff.dev@gmail.com>
2024-05-04 19:36:58 -0300

    feat: add shadcn/ui

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,3 +1,5 @@
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+
 interface NoteCardProps {
   date: Date;
   content: string;
@@ -6,11 +8,19 @@ interface NoteCardProps {
 
 export function NoteCard({date, content}:NoteCardProps ) {
   return (
-    <button className='flex flex-col text-left bg-slate-800 rounded-md p-5 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
-      <span className='text-slate-200' >{date.toISOString()}</span>
-      <p className='mt-3 text-slate-400' >{content}</p>
-    
-      <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-    </button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className='flex flex-col text-left bg-slate-800 rounded-md p-5 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+          <span className='text-slate-200' >{date.toISOString()}</span>
+          <p className='mt-3 text-slate-400' >{content}</p>
+        
+          <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
+        </button>
+      </DialogTrigger>
+
+      <DialogContent>
+        Teste
+      </DialogContent>
+    </Dialog>
   )
 }
