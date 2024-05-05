fc5bf874b2d530caa1b8be60923f8100eae8b4a6
EsdrasCFF <ecff.dev@gmail.com>
2024-05-05 01:03:30 -0300

    feat: add sonner

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,37 @@
+import { useState } from 'react'
 import logo from './assets/logo-my-notes.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+interface Note {
+  id: string;
+  date: Date;
+  content: string
+}
 
 export function App() {
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notes = localStorage.getItem('@my-notes/notes-array')
+    if(!notes) {
+      return []
+    }
+
+    return JSON.parse(notes)
+  })
+
+  function handleCreatedNote(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content
+    }
+
+    const notesArray = [newNote, ...notes]
+
+    setNotes(notesArray)
+
+    localStorage.setItem('@my-notes/notes-array', JSON.stringify(notesArray))
+  }
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -20,11 +48,10 @@ export function App() {
       <div className='h-px bg-slate-700'/>
 
       <div className='grid grid-cols-3 auto-rows-[250px] text-sm gap-6' >
-        <NewNoteCard />
-        <NoteCard date={new Date()} content='Uma nova historia Deus tem pra mim' />
-        <NoteCard date={new Date('04/04/2024')} content='Uma nova historia Deus tem pra mim' />
-        <NoteCard date={new Date('02/02/2024')} content='Um novo tempo DEus tem pra mim' />
-        <NoteCard date={new Date('01/01/2024')} content='Tudo aquilo que perdido for, ouvirei de sua boca' />
+        <NewNoteCard handleCreatedNote={handleCreatedNote} />
+        {notes.map((note) => (
+          <NoteCard content={note.content} date={note.date} key={note.id}  />
+        ))}
       </div>
       
     </div>
